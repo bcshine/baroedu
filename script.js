8df7aa61fe42b6fd08c33b66b10298b9ad1837ce
Your Name <your.email@example.com>
2025-07-03 18:56:01 +0900

    dddd

--- script.js
+++ script.js
@@ -908,7 +908,7 @@ async function loginWithGoogle() {
         const googleBtn = document.querySelector('.btn-social.google');
         if (googleBtn) {
             const originalText = googleBtn.innerHTML;
-            googleBtn.innerHTML = '<i class="fab fa-google"></i> 로그인 중...';
+            googleBtn.innerHTML = '로그인 중...';
             googleBtn.disabled = true;
             
             // 3초 후 원래 상태로 복원 (OAuth 리다이렉트가 실패한 경우 대비)
@@ -930,7 +930,7 @@ async function loginWithGoogle() {
             
             // 버튼 상태 복원
             if (googleBtn) {
-                googleBtn.innerHTML = '<i class="fab fa-google"></i> Google로 로그인';
+                googleBtn.innerHTML = '<svg width="18" height="18" viewBox="0 0 48 48"><path fill="#4285F4" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/><path fill="#34A853" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"/><path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/><path fill="#EA4335" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/></svg> Sign in with Google';
                 googleBtn.disabled = false;
             }
             
@@ -952,7 +952,7 @@ async function loginWithGoogle() {
         // 버튼 상태 복원
         const googleBtn = document.querySelector('.btn-social.google');
         if (googleBtn) {
-            googleBtn.innerHTML = '<i class="fab fa-google"></i> Google로 로그인';
+            googleBtn.innerHTML = '<svg width="18" height="18" viewBox="0 0 48 48"><path fill="#4285F4" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/><path fill="#34A853" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"/><path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/><path fill="#EA4335" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/></svg> Sign in with Google';
             googleBtn.disabled = false;
         }
         
